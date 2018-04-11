23f1a53faad17611dc17cb9c5ad2d654fe5b3116
Humberto Lozano <lchumberto90@gmail.com>
2018-04-11 16:48:37 -0500

    single web server practice

--- main.tf
+++ main.tf
@@ -1,14 +1,50 @@
+###  AWS PROVIDER ###
+
 provider "aws" {
-  /* normalmente usariamos las llaves de aws pero exportamos las credenciales
-       usando "export" por lo tanto no es necesario usarlas aqui */
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
+### EC2 SERVER ###
+
 resource "aws_instance" "example" {
-  ami           = "ami-name"
-  instance_type = "t2.micro"
+  ami                 = "ami-916f59f4"
+  instance_type       = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+		#!/bin/bash
+		echo "Hello, World" > index.html
+		nohup busybox httpd -f -p "${var.server_port}"
+		EOF
 
   tags {
-    Name = "terra-bb"
+    Name = "terrform-example"
   }
+}
+
+### AWS SG ###
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+### VARIABLES ###
+
+variable "server_port" {
+	description = "The port that we will use for HTTP requests"
+	default = "8080"
+}
+
+### OUTPUTS ###
+
+output "public_ip" {
+	value = "${aws_instance.example.public_ip}"
 }
